e2a45cbeb3b0635fcdd003ec68679c3b48b84161
Bhanu <sbhanupriya03@gmail.com>
2023-09-21 16:49:01 +0530

    Use of React Router for handling client-side routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
-    <Header />
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-light" data-bs-theme="light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">TODO APPLICATION</a>
+        <Link className="navbar-brand" to=''>TODO APPLICATION</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarColor03">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <a className="nav-link active" href="#">Home
+              <Link className="nav-link active" to='/'>Home
                 <span className="visually-hidden">(current)</span>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <Link className="nav-link" to='/login'>Login</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <Link className="nav-link" to='/register'>Register</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Logout</a>
+              <Link className="nav-link" to=''>Logout</Link>
             </li>
           </ul>
           <form className="d-flex">
